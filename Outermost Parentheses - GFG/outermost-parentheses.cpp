5156c37700c552796a0045f8c470774599f4d9e1
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-02-19 11:44:08 +0530

    Added solution - LeetHub

---
+++ Outermost Parentheses - GFG/outermost-parentheses.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string removeOuter(string& s) {
+        // code here
+        string ans;
+        int count=0;
+        
+        for(auto ch:s){
+            if(ch=='(' && count++>0)
+                ans+=ch;
+            if(ch==')' && count-->1)
+                ans+=ch;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+
+        cout << obj.removeOuter(s) << "\n";
+    }
+}
+// } Driver Code Ends
