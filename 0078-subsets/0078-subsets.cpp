ef8c97d823fd422f6c4797e1069519749151456d
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-02-23 12:35:29 +0530

    Time: 3 ms (62.29%), Space: 16.1 MB (23.48%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> ans;
     
-    void gen(vector<int> &nums,vector<int> &v,int i){
+    void gen(vector<int> &nums,vector<int> v,int i){
         if(v.size()==nums.size() || i==nums.size()){
             ans.push_back(v);
             return;
@@ -11,7 +11,6 @@ public:
         gen(nums,v,i+1);
         v.push_back(nums[i]);
         gen(nums,v,i+1);
-        v.pop_back();
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
