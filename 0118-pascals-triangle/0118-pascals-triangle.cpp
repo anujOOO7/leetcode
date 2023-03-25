536320e547da89fe1830325e1048dc04ad64bf9f
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-03-26 01:09:01 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (53.39%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>> ans;
+        vector<int> v;
         for(int i=0;i<n;i++)
         {
-            vector<int> v;
             if(i==0)
                 v.push_back(1);
             else
@@ -17,6 +17,7 @@ public:
                 v.push_back(1);
             }
             ans.push_back(v);
+            v.clear();
         }
         return ans;
     }
