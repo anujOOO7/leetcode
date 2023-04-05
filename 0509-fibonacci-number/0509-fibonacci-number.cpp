9b690213ba778274af40beabe83dcefaebeac6a3
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-04-05 12:48:00 +0530

    Time: 3 ms (71.03%), Space: 6 MB (20.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
+    int a[40];
+    int f(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(a[n]!=-1) return a[n];
+        return a[n]=f(n-1)+f(n-2);
+    }
+    
     int fib(int n) {
-        if(n==0)
-            return 0;
-        else if(n==1)
-            return 1;
-        return fib(n-1)+fib(n-2);
+        memset(a,-1,sizeof(a));
+        return f(n);
     }
 };
