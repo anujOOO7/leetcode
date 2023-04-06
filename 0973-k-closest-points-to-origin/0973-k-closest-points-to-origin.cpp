656ddf34a800a69d83e769540d4bd6e83d93a324
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-04-07 02:48:55 +0530

    Time: 224 ms (74.86%), Space: 63.3 MB (76.94%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        sort(points.begin(),points.end(),[](vector<int>& p, vector<int>& q) {
-            return p[0]*p[0]+p[1]*p[1]<q[0]*q[0]+q[1]*q[1];
-        });
-        return vector<vector<int>>(points.begin(),points.begin()+k);
+        vector<vector<int>> ans;
+        priority_queue<pair<int,pair<int,int>>>pq;
+        for(int i=0;i<points.size();i++)
+        {
+            int dis=pow(points[i][0],2)+pow(points[i][1],2);
+            pq.push({dis,{points[i][0],points[i][1]}});
+            if(pq.size()>k) 
+            {             
+                pq.pop();
+            }
+        }
+        while(!pq.empty())
+        {
+            ans.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
+        }
+        return ans;
     }
 };
