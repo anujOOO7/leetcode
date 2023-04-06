b97a928f2baa9cfdcd3c58d196251c9d922b82ee
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-04-07 02:36:45 +0530

    Time: 253 ms (56.06%), Space: 56.9 MB (98.05%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,28 +1,9 @@
-bool cmp(pair<int,pair<int,int>> &a,pair<int,pair<int,int>> &b){
-    long double x,y;
-    x=sqrt(a.second.first*a.second.first+a.second.second*a.second.second);
-    y=sqrt(b.second.first*b.second.first+b.second.second*b.second.second);
-    return x<y;
-}
-
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k){
-        vector<vector<int>> ans;
-        vector<pair<int,pair<int,int>>> v;
-        int ind=0;
-        for(auto i:points)
-        {
-            v.push_back({ind,{i[0],i[1]}});
-            ind++;
-        }
-        sort(v.begin(),v.end(),cmp);
-        for(auto i:v)
-        {
-            --k;
-            ans.push_back(points[i.first]);
-            if(k<=0) break;
-        }
-        return ans;
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        sort(points.begin(),points.end(),[](vector<int>& p, vector<int>& q) {
+            return p[0]*p[0]+p[1]*p[1]<q[0]*q[0]+q[1]*q[1];
+        });
+        return vector<vector<int>>(points.begin(),points.begin()+k);
     }
 };
