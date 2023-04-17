fb2a04001a180157b1cc40330150e81e526a220c
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-04-17 06:17:28 +0530

    Time: 0 ms (100.00%), Space: 6 MB (50.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    int a[46];
-    int f(int n){
-        if(n==0||n==1)
-            return 1;
-        if(a[n]!=-1) 
-            return a[n];
-        return a[n]=f(n-1)+f(n-2);
-    }
-    
     int climbStairs(int n) {
-        memset(a,-1,sizeof(a));
-        return f(n);
+        int a[n+1];
+        a[0]=a[1]=1;
+        for(int i=2;i<=n;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        return a[n];
     }
 };
