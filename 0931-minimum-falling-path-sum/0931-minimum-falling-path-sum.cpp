e559b388a50d78f61650e92e053bd5afc35df602
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-05-09 00:49:41 +0530

    Time: 18 ms (38.94%), Space: 9.8 MB (97.31%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -3,30 +3,25 @@ public:
     int dp[101][101];
     
     int f(int i,int j,vector<vector<int>>& m){
-        if(j>=m[0].size() || j<0)
-            return 1e5;
-        if(i==m.size()-1)
-            return m[i][j];
-        if(i>=m.size())
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+        int n=m.size();
+        if(i>=n) return 0;
+        if(j<0 || j+1>n) return 1e5;
+        if(i==n-1) return m[i][j];
+        
+        if(dp[i][j]!=-1) return dp[i][j];
         int ans=INT_MAX;
         ans=min(ans,m[i][j]+f(i+1,j-1,m));
         ans=min(ans,m[i][j]+f(i+1,j,m));
         ans=min(ans,m[i][j]+f(i+1,j+1,m));
         
-      return dp[i][j]=ans;
-        
-    
-        
+        return dp[i][j]=ans;
     }
-    int minFallingPathSum(vector<vector<int>>& m) {
+    int minFallingPathSum(vector<vector<int>>& m) 
+    {
         memset(dp,-1,sizeof(dp));
         int ans=INT_MAX;
-        for(int i=0;i<m[0].size();i++)
-        {
-            ans=min(ans,f(0,i,m));
+        for(int j=0;j<m.size();j++){
+            ans=min(ans,f(0,j,m));
         }
         return ans;
     }
