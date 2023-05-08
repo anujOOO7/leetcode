1cc6192771c5bedc5bffff1a3ea0f93ea2704fc7
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-05-09 01:24:18 +0530

    Time: 75 ms (86.96%), Space: 11.4 MB (86.14%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[13][10001];
+    int f(int i,vector<int>& c,int a){
+        if(a==0) return 0;
+        if(a!=0 && i==c.size()) return 1e5;
+        if(dp[i][a]!=-1)
+            return dp[i][a];
+        
+        if(a<c[i]) 
+        return dp[i][a]=f(i+1,c,a);
+        else
+        return dp[i][a]=min(1+f(i,c,a-c[i]),f(i+1,c,a));
+    }
+    int coinChange(vector<int>& c,int a){
+        sort(rbegin(c),rend(c));
+        memset(dp,-1,sizeof(dp));
+        int ans=f(0,c,a);
+        if(ans>=1e5) return -1;
+        return ans;
+    }
+};
