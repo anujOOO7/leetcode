b3e1b77b8a765b6dcb4f1987d7a6b0633779b3b2
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-05-25 17:40:49 +0530

    Time: 62 ms (33.71%), Space: 10.9 MB (78.30%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -17,16 +17,19 @@ public:
         // memset(dp,-1,sizeof(dp));
         // return f(n,m);
         
-        double dp[n+1][m+1];
-        memset(dp,0,sizeof(dp));
-        for(int i=0;i<=n;i++) dp[i][0]=1;
+        // double dp[n+1][m+1];
+        // memset(dp,0,sizeof(dp));
+        // for(int i=0;i<=n;i++) dp[i][0]=1;
+        vector<double> cur(m+1,0),prev(m+1,0);
+        cur[0]=prev[0]=1;
         
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                else dp[i][j]=dp[i-1][j];
+                if(s[i-1]==t[j-1]) cur[j]=prev[j-1]+prev[j];
+                else cur[j]=prev[j];
             }
+            prev=cur;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
