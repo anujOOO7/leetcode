79284fca99d8e65e4acf5a16c54731fc2e779857
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-05-25 23:29:03 +0530

    Time: 97 ms (9.39%), Space: 7.4 MB (79.65%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,17 +4,17 @@ public:
     string s1,s2;
     int dp[501][501];
     int f(int i,int j){
-        if(j<0) return i+1;
-        if(i<0) return j+1;
+        if(j==0) return i;
+        if(i==0) return j;
         
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]) return dp[i][j]=f(i-1,j-1);
+        if(s1[i-1]==s2[j-1]) return dp[i][j]=f(i-1,j-1);
         return dp[i][j]=min(1+f(i,j-1),min(1+f(i-1,j),1+f(i-1,j-1)));
     }
     int minDistance(string word1, string word2) {
         s1=word1,s2=word2;
         n=s1.length(),m=s2.length();
         memset(dp,-1,sizeof(dp));
-        return f(n-1,m-1);
+        return f(n,m);
     }
 };
