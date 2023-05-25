bd08c8fa8705ae9872d1b35e184f1c0e34dc5183
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-05-25 17:35:37 +0530

    Time: 41 ms (75.03%), Space: 19.5 MB (45.09%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -14,7 +14,19 @@ public:
     int numDistinct(string str1, string str2) {
         s=str1,t=str2;
         n=s.size(),m=t.size();
-        memset(dp,-1,sizeof(dp));
-        return f(n,m);
+        // memset(dp,-1,sizeof(dp));
+        // return f(n,m);
+        
+        double dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return (int)dp[n][m];
     }
 };
