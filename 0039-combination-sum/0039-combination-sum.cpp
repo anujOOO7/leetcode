5c6411955407a2ae23d87c28a2bfd68977958e7d
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-05-30 20:08:38 +0530

    Time: 4 ms (82.56%), Space: 10.8 MB (73.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
+    int target;
+    vector<int> c;
     vector<vector<int>> ans;
-    
-    void gen(vector<int> &c,vector<int> &temp,int i,int target){
-        if(target==0){
+    void f(int i,int sum,vector<int> &temp){
+        if(sum==target){
             ans.push_back(temp);
-            return ;
-        }
-        
-        if(target<0)
             return;
-        
-        if(i==c.size())
-            return ;
-        
-        //not inserting
-        gen(c,temp,i+1,target);
+        }
+        if(i==c.size() || sum>target) return;
         
         //inserting candidates
         temp.push_back(c[i]);
-        gen(c,temp,i,target-c[i]);
+        f(i,sum+c[i],temp);
         temp.pop_back();
+        
+        //not inserting
+        f(i+1,sum,temp);
     }
     
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int t) {
         vector<int> temp;
-        gen(candidates,temp,0,target);
+        c=candidates,target=t;
+        f(0,0,temp);
         return ans;
     }
 };
