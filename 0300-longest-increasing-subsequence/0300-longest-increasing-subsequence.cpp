29b4a5b871581bf2dff489fbd241076bf0f70bb9
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-06-06 00:57:03 +0530

    Time: 636 ms (29.47%), Space: 10.7 MB (33.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> nums;
-    int dp[2501][2501];
+    int dp[2501];
     int f(int i,int prev_ind){
         if(i==nums.size()) return 0;
         
-        int &d=dp[i][prev_ind+1];
+        int &d=dp[prev_ind+1];
         if(d!=-1) return d;
         int len=f(i+1,prev_ind);
         if(prev_ind==-1 || nums[i]>nums[prev_ind]){
@@ -13,18 +13,9 @@ public:
         }
         return d=len;
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        int dp[n+1][n+1];
-        memset(dp,0,sizeof(dp));
-        
-        for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=-1;j--){
-                int count=dp[i+1][j+1];
-                if(j==-1 || nums[i]>nums[j]) count=max(count,1+dp[i+1][i+1]);
-                dp[i][j+1]=count;
-            }
-        }
-        return dp[0][0];
+    int lengthOfLIS(vector<int>& arr) {
+        memset(dp,-1,sizeof(dp));
+        nums=arr;
+        return f(0,-1);
     }
 };
