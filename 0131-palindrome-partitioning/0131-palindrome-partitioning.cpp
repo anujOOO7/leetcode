c0c36d526e86f33aeb21bc65e7ee62be6b7990ce
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-06-08 18:18:25 +0530

    Time: 159 ms (32.21%), Space: 79.9 MB (29.11%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> res;
-    
     bool isPalindrome(string &s,int i,int j){
         while(i<=j){
             if(s[i++]!=s[j--]) return false;
         }
         return true;
     }
-    
-    void gen(string s,vector<string> &temp,int index){
+    void f(string s,vector<string> &temp,int index){
         if(index==s.size()){
             res.push_back(temp);
             return ;
         }
         
         for(int i=index;i<s.size();i++){
+            //if s.substr(index,i-index+1) is palindrome then move for next steps
             if(isPalindrome(s,index,i)){
                 temp.push_back(s.substr(index,i-index+1));
-                gen(s,temp,i+1);
+                f(s,temp,i+1);
                 temp.pop_back();
             }
         }
@@ -26,7 +25,7 @@ public:
     
     vector<vector<string>> partition(string s) {
         vector<string> temp;
-        gen(s,temp,0);
+        f(s,temp,0);
         return res;
     }
 };
