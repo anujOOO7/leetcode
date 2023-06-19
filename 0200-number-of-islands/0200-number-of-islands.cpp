694913ea554b906a2e850f3ad9981b60ace9ba65
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-06-19 15:08:28 +0530

    Time: 39 ms (74.33%), Space: 13.3 MB (43.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int n,m;
-    void bfs(int i,int j,vector<vector<int>>& vis,vector<vector<char>>& grid){
+    void dfs(int i,int j,vector<vector<int>>& vis,vector<vector<char>>& grid){
         vis[i][j]=1;
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        
-        while(!q.empty()){
-            i=q.front().first;
-            j=q.front().second;
-            q.pop();
-            
-            for(int di=-1;di<=1;di++){
-                for(int dj=-1;dj<=1;dj++){
-                    if(abs(di)+abs(dj)!=2){
-                        int ni=i+di,nj=j+dj;
-                        if(ni>=0 && nj>=0 && ni<n && nj<m && grid[ni][nj]=='1' && !vis[ni][nj]) bfs(ni,nj,vis,grid);
-                    }
+ 
+        for(int di=-1;di<=1;di++){
+            for(int dj=-1;dj<=1;dj++){
+                if(abs(di)+abs(dj)!=2){
+                    int ni=i+di,nj=j+dj;
+                    if(ni>=0 && nj>=0 && ni<n && nj<m && grid[ni][nj]=='1' && !vis[ni][nj]) dfs(ni,nj,vis,grid);
                 }
             }
         }
@@ -29,7 +21,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1' && !vis[i][j]){
-                    bfs(i,j,vis,grid);
+                    dfs(i,j,vis,grid);
                     cnt++;
                 }
             }
