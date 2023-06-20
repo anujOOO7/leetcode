d60fd669b95b3f8ec2495f8802ea764af198961f
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-06-20 10:06:42 +0530

    Time: 1181 ms (17.40%), Space: 8.1 MB (43.81%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int m,n;
-    vector<int> dx{-1,0,0,1};
-    vector<int> dy{0,-1,1,0};
-    bool f(int i,int j,int k,string word,vector<vector<char>>& board){
+    vector<vector<int>> vis;
+    bool dfs(int i,int j,int k,string word,vector<vector<char>>& board){
         if(k==word.size()) return true;
-        if(i<0 || i>=m || j<0 || j>=n || board[i][j]!=word[k]) return false;
+        if(i<0 || i>=m || j<0 || j>=n) return false;
         
-        char temp=board[i][j];
-        board[i][j]='@';
-        for(int p=0;p<4;p++){
-            int ni=i+dx[p],nj=j+dy[p];
-            if(f(ni,nj,k+1,word,board)) return true;
+        if(board[i][j]==word[k] && !vis[i][j]){
+            vis[i][j]=1;
+            if(dfs(i-1,j,k+1,word,board) || dfs(i+1,j,k+1,word,board) || dfs(i,j-1,k+1,word,board) || dfs(i,j+1,k+1,word,board)) return true;
+            else{
+                vis[i][j]=0;
+                return false;
+            }
         }
-        board[i][j]=temp;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         m=board.size(),n=board[0].size();
+        vis.resize(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                    if(f(i,j,0,word,board)) return true;
+                    if(dfs(i,j,0,word,board)) return true;
                 }
             }
         }
