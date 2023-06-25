8c49c480aa3406f811a73aa053c11b48ca003749
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-06-25 21:24:07 +0530

    Time: 15 ms (74.31%), Space: 9.1 MB (65.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+    int dp[21][2002];
     int f(int i,int sum,int t,vector<int>& nums){
         if(i==nums.size()){
             if(sum==t) return 1;
             else return 0;
         }
         
-        return f(i+1,sum-nums[i],t,nums)+f(i+1,sum+nums[i],t,nums);
+        int &d=dp[i][sum+1000];
+        if(d!=-1) return d;
+        return d=f(i+1,sum-nums[i],t,nums)+f(i+1,sum+nums[i],t,nums);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
         return f(0,0,target,nums);
     }
 };
