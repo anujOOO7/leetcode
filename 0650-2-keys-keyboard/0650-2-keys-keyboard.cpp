0176dcb4c1e1e64f17406ef2d5cbeffaaa893164
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-06-25 21:37:59 +0530

    Time: 52 ms (20.68%), Space: 10.7 MB (23.37%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    int dp[1001][1001];
     int f(int count,int copied,int n){
         if(count>n || copied>n) return 1e6;
         if(count==n) return 1;
         
-        return min({1+f(count+copied,copied,n),2+f(2*count,count,n)});
+        int &d=dp[count][copied];
+        if(d!=-1) return d;
+        return d=min({1+f(count+copied,copied,n),2+f(2*count,count,n)});
     }
     int minSteps(int n) {
         if(n==1) return 0;
+        memset(dp,-1,sizeof(dp));
         return f(1,1,n);
     }
 };
