525f49d8c2bdb6c7f86769a9bcdb0129aaf35858
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-06-30 21:10:55 +0530

    Time: 44 ms (7.19%), Space: 16.8 MB (6.42%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,13 +1,39 @@
-class Solution {
-public:
-    //dfs traversal
-    void dfs(int node,vector<int>& vis,vector<int> adj[]){
-        vis[node]=1;
-        for(auto it:adj[node]){
-            if(!vis[it]) dfs(it,vis,adj);
-        }
+class DisjointSet{
+    public:
+    vector<int> parent,size;
+    
+    //constructor
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++) parent[i]=i;
     }
     
+    //function to find the ultimate parent of any node
+    int ultParent(int u){
+        if(u==parent[u]) return u;
+        return parent[u]=ultParent(parent[u]);
+    }
+    
+    //union by size
+    void unionBySize(int u,int v){
+        int ult_u=ultParent(u);
+        int ult_v=ultParent(v);
+        
+        if(ult_u==ult_v) return;
+        if(size[ult_u]<size[ult_v]){
+            size[ult_v]+=size[ult_u];
+            parent[ult_u]=ult_v;
+        }
+        else{
+            size[ult_u]+=size[ult_v];
+            parent[ult_v]=ult_u;
+        }
+    }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& a) {
         int n=a.size();
         vector<int> adj[n];
@@ -22,15 +48,21 @@ public:
             }
         }
         
-        //check for connected components
-        int ans=0;
-        vector<int> vis(n,0);
+        DisjointSet ds(n);
+        //union by size
         for(int i=0;i<n;i++){
-            if(!vis[i]){ 
-                dfs(i,vis,adj);
-                ans++;
+            for(auto it:adj[i]){
+                if(ds.ultParent(i)!=ds.ultParent(it)){
+                    ds.unionBySize(i,it);
+                }
             }
         }
-        return ans;
+        
+        //counting for the ultimate parents i.e. no. of provinces
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(i==ds.parent[i]) cnt++;
+        }
+        return cnt;
     }
 };
