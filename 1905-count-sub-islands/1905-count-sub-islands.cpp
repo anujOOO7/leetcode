d3154590580d2a1052dbc9c7b69613b974e4c94d
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-07-02 17:39:26 +0530

    Time: 364 ms (65.84%), Space: 108.8 MB (22.92%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int m,n;
-    bool dfs(int i,int j,vector<vector<int>>& grid1,vector<vector<int>>& grid2){
-        if(i<0 ||i>=m || j<0 || j>=n || grid2[i][j]!=1) return true;
-        if(grid1[i][j]==0) return false;
-        grid2[i][j]=-1;
-        bool ans=true;
-        ans&=dfs(i+1,j,grid1,grid2);
-        ans&=dfs(i-1,j,grid1,grid2);
-        ans&=dfs(i,j-1,grid1,grid2);
-        ans&=dfs(i,j+1,grid1,grid2);
-        return ans;
+    vector<vector<int>> a,b;
+    vector<vector<int>> vis;
+    bool dfs(int i,int j){
+        if(i<0 ||i>=m || j<0 || j>=n || !b[i][j] || vis[i][j]) return true;
+        if(!a[i][j]) return false;
+        vis[i][j]=1;
+        return dfs(i+1,j)&dfs(i-1,j)&dfs(i,j-1)&dfs(i,j+1);
     }
     
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        a=grid1,b=grid2;
         m=grid2.size(),n=grid2[0].size();
+        vis.resize(m,vector<int>(n,0));
         int cnt=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid2[i][j]==1){
-                    if(dfs(i,j,grid1,grid2)) cnt++;
+                if(grid2[i][j]==1 && !vis[i][j]){
+                    if(dfs(i,j)) cnt++;
                 }
             }
         }
