1a0233690d7a967ea2648331b714981bdac666af
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-07-02 19:29:21 +0530

    Added solution - LeetHub

--- Number Of Islands - GFG/number-of-islands.cpp
+++ Number Of Islands - GFG/number-of-islands.cpp
@@ -6,67 +6,65 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-class DisjointSet {
-public:
+class DisjointSet{
+    public:
     vector<int> parent,size;
-    DisjointSet(int n) {
-        parent.resize(n+1,1);
+    
+    DisjointSet(int n){
+        parent.resize(n+1);
         size.resize(n+1,1);
         for(int i=0;i<=n;i++) parent[i]=i;
     }
-
-    int findUPar(int node) {
-        if(node==parent[node]) return node;
-        return parent[node]=findUPar(parent[node]);
+    
+    int find(int u){
+        if(u==parent[u]) return u;
+        return parent[u]=find(parent[u]);
     }
     
     void unionBySize(int u,int v){
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u]<size[ulp_v]) swap(ulp_u,ulp_v);
-        parent[ulp_v] = ulp_u;
-        size[ulp_u] += size[ulp_v];
+        int uu=find(u);
+        int uv=find(v);
+        if(uu==uv) return;
+        if(size[uu]<size[uv]) swap(uu,uv);
+        size[uu]+=size[uv];
+        parent[uv]=uu;
     }
 };
 
 class Solution {
-private:
-    bool isValid(int adjr, int adjc, int n, int m) {
-        return adjr >= 0 && adjr < n && adjc >= 0 && adjc < m;
-    }
-public:
-    vector<int> numOfIslands(int n,int m,vector<vector<int>> &operators) {
-        DisjointSet ds(n * m);
+  public:
+    vector<int> dx{-1,0,1,0};
+    vector<int> dy{0,-1,0,1};
+    vector<int> numOfIslands(int n, int m, vector<vector<int>> &operators) {
+        // code here
+        DisjointSet ds(n*m);
         int vis[n][m];
-        memset(vis, 0, sizeof vis);
-        int cnt = 0;
+        memset(vis,0,sizeof(vis));
+        int cnt=0;
         vector<int> ans;
-        for (auto it : operators) {
-            int row = it[0];
-            int col = it[1];
-            if (vis[row][col] == 1) {
+        
+        for(auto it:operators){
+            int r=it[0],c=it[1];
+            if(vis[r][c]){
                 ans.push_back(cnt);
                 continue;
             }
-            vis[row][col] = 1;
+            vis[r][c]=1;
             cnt++;
-            // row - 1, col
-            // row , col + 1
-            // row + 1, col
-            // row, col - 1;
-            int dr[] = { -1, 0, 1, 0};
-            int dc[] = {0, 1, 0, -1};
-            for (int ind = 0; ind < 4; ind++) {
-                int adjr = row + dr[ind];
-                int adjc = col + dc[ind];
-                if (isValid(adjr, adjc, n, m)) {
-                    if (vis[adjr][adjc] == 1) {
-                        int nodeNo = row * m + col;
-                        int adjNodeNo = adjr * m + adjc;
-                        if (ds.findUPar(nodeNo) != ds.findUPar(adjNodeNo)) {
+            
+            for(int i=0;i<4;i++){
+                int ni=r+dx[i];
+                int nj=c+dy[i];
+                
+                if(ni>=0 && ni<n && nj>=0 && nj<m){
+                    if(vis[ni][nj]){
+                        int a=r*m+c;
+                        int adj=ni*m+nj;
+                        
+                        //if they are not connected
+                        if(ds.find(a)!=ds.find(adj)){
                             cnt--;
-                            ds.unionBySize(nodeNo, adjNodeNo);
+                            ds.unionBySize(a,adj);
                         }
                     }
                 }
