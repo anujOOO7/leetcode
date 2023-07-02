ca60926340c7b21e55682d2402fc95f9fa229452
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-07-02 20:53:13 +0530

    Time: 515 ms (61.16%), Space: 105.4 MB (82.53%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -0,0 +1,88 @@
+class DisjointSet{
+    public:
+    vector<int> parent,size;
+    
+    //constructor
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++) parent[i]=i;
+    }
+    
+    //function to find the ultimate parent of any node
+    int find(int u){
+        if(u==parent[u]) return u;
+        return parent[u]=find(parent[u]);
+    }
+    
+    //union by size
+    void unionBySize(int u,int v){
+        int ult_u=find(u);
+        int ult_v=find(v);
+        
+        if(ult_u==ult_v) return;
+        if(size[ult_u]<size[ult_v]){
+            size[ult_v]+=size[ult_u];
+            parent[ult_u]=ult_v;
+        }
+        else{
+            size[ult_u]+=size[ult_v];
+            parent[ult_v]=ult_u;
+        }
+    }
+};
+
+class Solution {
+bool valid(int i,int j,int n){
+    return i>=0 && i<n && j>=0 && j<n;
+}
+public:
+    vector<int> dx{-1,0,0,1};
+    vector<int> dy{0,-1,1,0};
+    int largestIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+        DisjointSet ds((n*n));
+        
+        //union by size
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0) continue;
+                for(int p=0;p<4;p++){
+                    int ni=i+dx[p];
+                    int nj=j+dy[p];
+                    
+                    if(valid(ni,nj,n) && grid[ni][nj]==1){
+                        int oldN=i*n+j;
+                        int newN=ni*n+nj;
+                        ds.unionBySize(oldN,newN);
+                    }
+                }
+            }
+        }
+        
+        //checking maxSize by converting each 0 to 1 one by one
+        int res=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) continue;
+                set<int> s;
+                for(int p=0;p<4;p++){
+                    int ni=i+dx[p];
+                    int nj=j+dy[p];
+
+                    if(valid(ni,nj,n) && grid[ni][nj]==1){
+                        s.insert(ds.find(ni*n+nj));
+                    }
+                }
+
+                int ans=1;
+                for(auto it:s){
+                    ans+=ds.size[it];
+                }
+                res=max(res,ans);
+            }
+        }
+        for(int i=0;i<n*n;i++) res=max(res,ds.size[i]);
+        return res;
+    }
+};
