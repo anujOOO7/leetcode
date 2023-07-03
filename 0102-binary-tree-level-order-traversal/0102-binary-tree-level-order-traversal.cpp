7747e3e3206247d892b34459b5ffef9e5f64d0fd
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-07-03 23:23:41 +0530

    Time: 19 ms (5.64%), Space: 14 MB (9.29%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,19 +11,34 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode *root,vector<vector<int>> &ans,int d){
-        if(!root) return;
-        if(d>=ans.size()) ans.push_back({});
-        
-        ans[d].push_back(root->val);
-        
-        dfs(root->left,ans,d+1);
-        dfs(root->right,ans,d+1);
-    }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(!root) return ans;
-        dfs(root,ans,0);
+        if(!root) return {};
+        vector<vector<int>> ans;
+        queue<vector<TreeNode*>> q;
+        ans.push_back({root->val});
+        q.push({root});
+        
+        while(!q.empty()){
+            vector<TreeNode*> temp;
+            vector<int> data;
+            auto node=q.front();
+            q.pop();
+            
+            for(auto it:node){
+                if(it->left){
+                    temp.push_back(it->left);
+                    data.push_back(it->left->val);
+                }
+                if(it->right){
+                    temp.push_back(it->right);
+                    data.push_back(it->right->val);
+                }
+            }
+            if(temp.size()) q.push(temp);
+            if(data.size()) ans.push_back(data);
+            temp.clear();
+            data.clear();
+        }
         return ans;
     }
 };
