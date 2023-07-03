76a4739eb2d31965b1f95e43c66febb8a089c149
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-07-04 00:50:12 +0530

    Time: 119 ms (91.24%), Space: 52.2 MB (59.70%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -4,9 +4,9 @@ public:
         deque<int> mx,mn;
         int i=0,j;
         for(j=0;j<nums.size();j++){
-            while(!mx.empty() && nums[j] > mx.back()) mx.pop_back();
+            while(!mx.empty() && nums[j]>mx.back()) mx.pop_back();
             mx.push_back(nums[j]);
-            while(!mn.empty() && nums[j] < mn.back()) mn.pop_back();
+            while(!mn.empty() && nums[j]<mn.back()) mn.pop_back();
             mn.push_back(nums[j]);
             if(mx.front()-mn.front()>limit){
                 if(mx.front()==nums[i]) mx.pop_front();
