23b0136f98fcfa607e3e9f70ee4ffc1a624c1470
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-07-15 14:20:58 +0530

    Time: 328 ms (71.72%), Space: 79.6 MB (53.08%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> dp;
+    vector<int> nums;
     int f(int i,int k,vector<vector<int>> &events){
         if(i>=events.size()) return 0;
         if(k<=0) return 0;
@@ -8,8 +9,7 @@ public:
         int &d=dp[i][k];
         if(d!=-1) return d;
         int ans=f(i+1,k,events);
-        vector<int> temp={events[i][1],INT_MAX,INT_MAX};
-        auto j=upper_bound(events.begin(),events.end(),temp)-events.begin();
+        auto j=upper_bound(nums.begin(),nums.end(),events[i][1])-nums.begin();
         ans=max(ans,events[i][2]+f(j,k-1,events));
         return d=ans;
     }
@@ -17,6 +17,7 @@ public:
     int maxValue(vector<vector<int>>& events, int k) {
         dp.resize(events.size()+1,vector<int>(k+1,-1));
         sort(events.begin(),events.end());
+        for(auto it:events) nums.push_back(it[0]);
         // for(auto it:events) cout<<it[0]<<" "<<it[1]<<" "<<it[2]<<endl;
         return f(0,k,events);
     }
