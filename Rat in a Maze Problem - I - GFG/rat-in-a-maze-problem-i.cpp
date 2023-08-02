b8d3f37aae1a346255367a89b10f0797ef6055d1
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-08-03 02:26:45 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -8,53 +8,29 @@ using namespace std;
 // } Driver Code Ends
 // User function template for C++
 
-
-void sol(vector<vector<int> > &m,int i,int j,int n,vector<vector<int>> visited,string s,vector<string>& ans){
-  if(i==n-1 && j==n-1){
-    ans.push_back(s);
-    return;
-  }
-
-  //down
-  if(i+1<n && !visited[i+1][j] && m[i+1][j]==1){
-    visited[i+1][j]=1;
-    sol(m,i+1,j,n,visited,s+'D',ans);
-    visited[i+1][j]=0;
-  }
-
-  //left
-  if(j-1>=0 && !visited[i][j-1] && m[i][j-1]==1){
-    visited[i][j-1]=1;
-    sol(m,i,j-1,n,visited,s+'L',ans);
-    visited[i][j-1]=0;
-  }
-
-  //right
-  if(j+1<n && !visited[i][j+1] && m[i][j+1]==1){
-    visited[i][j+1]=1;
-    sol(m,i,j+1,n,visited,s+'R',ans);
-    visited[i][j+1]=0;
-  }
-
-  //up
-  if(i-1>=0 && !visited[i-1][j] && m[i-1][j]==1){
-    visited[i-1][j]=1;
-    sol(m,i-1,j,n,visited,s+'U',ans);
-    visited[i-1][j]=0;
-  }
-}
-
 class Solution{
     public:
-    vector<string> findPath(vector<vector<int>> &m, int n) {
-        // Your code goes here
-        //i,j -> n-1,n-1 visited ans 
-        vector<string> ans;
-        vector<vector<int>> visited(n,vector<int> (n,0));
-          visited[0][0]=1;
-          if(m[0][0]==1) sol(m,0,0,n,visited,"",ans);
-        //   sort(begin(ans),end(ans));
-          return ans;
+    int n;
+    vector<string> ans;
+    void f(int i,int j,string s,vector<vector<int>> &m,vector<vector<int>> &vis){
+        if(i<0 || i>=n || j<0 || j>=n || vis[i][j] || !m[i][j]) return;
+        if(i==n-1 && j==n-1){
+            ans.push_back(s);
+            return;
+        }
+        vis[i][j]=1;
+        
+        f(i-1,j,s+'U',m,vis);
+        f(i+1,j,s+'D',m,vis);
+        f(i,j-1,s+'L',m,vis);
+        f(i,j+1,s+'R',m,vis);
+        vis[i][j]=0;
+    }
+    vector<string> findPath(vector<vector<int>> &m, int N) {
+        n=N;
+        vector<vector<int>> vis(n,vector<int>(n,0));
+        f(0,0,"",m,vis);
+        return ans;
     }
 };
 
