2401dffdb832f5210e4ffcf5ceece3d6b46cc34e
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-09-15 16:12:09 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,100 +3,35 @@
 using namespace std;
 
 // } Driver Code Ends
-
-class DisjointSet{
-    vector<int> parent,rank,size;
-    public:
-    
-    DisjointSet(int n){
-        parent.resize(n+1);
-        rank.resize(n+1,0);
-        size.resize(n+1,1);
-        
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
-    }
-    
-    //finding ultimate parent of a given node
-    int ultParent(int node){
-        if(node==parent[node]) return node;
-        return parent[node]=ultParent(parent[node]);
-    }
-    
-    //union by size
-    void unionBySize(int u,int v){
-        int ult_u=ultParent(u);
-        int ult_v=ultParent(v);
-        
-        if(ult_u==ult_v) return;
-        if(size[ult_u]<size[ult_v]){
-            parent[ult_u]=ult_v;
-            size[ult_v]+=size[ult_u];
-        }
-        else{
-            parent[ult_v]=ult_u;
-            size[ult_u]+=size[ult_v];
-        }
-    }
-};
-
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        //__________________PRIM'S ALGORITHM_______________
-        // vector<int> vis(V,0);
-        // priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        // pq.push({0,0});
+        vector<int> vis(V,0);
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        pq.push({0,0});
         
-        // int sum=0;
-        // while(!pq.empty()){
-        //     int wt=pq.top()[0];
-        //     int node=pq.top()[1];
-        //     pq.pop();
+        int sum=0;
+        while(!pq.empty()){
+            int wt=pq.top()[0];
+            int node=pq.top()[1];
+            pq.pop();
             
-        //     if(vis[node]) continue;
-        //     vis[node]=1;
-        //     sum+=wt;
-        //     for(auto it:adj[node]){
-        //         if(!vis[it[0]]){
-        //             pq.push({it[1],it[0]});
-        //         }
-        //     }
-        // }
-        // return sum;
-        
-        
-        //_______________KRUSKAL'S ALGO_____________________
-        
-        DisjointSet ds(V);
-        vector<vector<int>> edges;
-        for(int i=0;i<V;i++){
-            for(auto it:adj[i]){
-                int node=i;
-                int adjNode=it[0];
-                int wt=it[1];
-                
-                edges.push_back({wt,node,adjNode});
+            if(vis[node]) continue;
+            vis[node]=1;
+            sum+=wt;
+            for(auto it:adj[node]){
+                if(!vis[it[0]]){
+                    pq.push({it[1],it[0]});
+                }
             }
         }
-        
-        sort(edges.begin(),edges.end());
-        
-        int res=0;
-        for(auto it:edges){
-            int wt=it[0];
-            if(ds.ultParent(it[1])!=ds.ultParent(it[2])){
-                ds.unionBySize(it[1],it[2]);
-                res+=wt;
-            }
-        }
-        return res;
+        return sum;
     }
 };
+
 
 //{ Driver Code Starts.
 
