a363cb547aa8156c1552d69841c76ccbfd766b2b
Anuj Kumar <113918728+anujOOO7@users.noreply.github.com>
2023-10-23 13:48:00 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -5,29 +5,17 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
-	int dp[1001][1010];
-	int f(int i,int prev_ind,int n,int arr[]){
-	    if(i==n) return 0;
-	    
-	    int &d=dp[i][prev_ind+1];
-	    if(d!=-1) return d;
-	    
-	    int take=0;
-	    if(prev_ind==-1 || arr[prev_ind]<arr[i]) take=max(take,arr[i]+f(i+1,i,n,arr));
-	    int ntake=f(i+1,prev_ind,n,arr);
-	    return d=max(take,ntake);
-	}
 	int maxSumIS(int arr[], int n)  
-	{  
-	   // memset(dp,-1,sizeof(dp));
-	   // return f(0,-1,n,arr);
+	{
 	    int ans[n]; 
-        for (int i = 0; i < n; i++) ans[i] = arr[i];  
-        for (int i = 0; i < n; i++)  
-            for (int j = 0; j < i; j++)  
-                if (arr[i] > arr[j] && ans[i] < ans[j] + arr[i])  
-                    ans[i] = ans[j] + arr[i];  
-    
+        for(int i=0;i<n;i++) ans[i]=arr[i];  
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i]>arr[j] && ans[i]<ans[j]+arr[i]){
+                    ans[i]=ans[j]+arr[i];
+                }
+            }
+        }
         return *max_element(ans, ans + n);  
 	}
 };
